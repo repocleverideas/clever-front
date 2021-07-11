010dcebc0f74e4e3abc6c93f071b6e56412ebfc2
Eduardo Diaz <lalo.diaz1896@gmail.com>
2021-07-10 19:51:10 -0500

    :lipstick: [ADD] Legal pages

---
+++ pages/privacidad.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Header, Footer } from '../components'
+import ReactMarkdown from 'react-markdown'
+import styles from '../styles/pua/pua.module.css'
+
+function privacidad(data) {
+  return (
+    <>
+      <Header />
+      <section className={`column ${styles.background}`}>
+        <h1 className={styles.title}>{data.title}</h1>
+        <ReactMarkdown>{data.text}</ReactMarkdown>
+      </section>
+
+      <Footer />
+    </>
+  )
+}
+
+export default privacidad
+
+export async function getStaticProps({ locale }) {
+  // http://localhost:1337/home?_locale=en
+  const res = await fetch(`http://localhost:1337/privacidad`)
+  const data = await res.json()
+
+  return {
+    props: data
+  }
+}
